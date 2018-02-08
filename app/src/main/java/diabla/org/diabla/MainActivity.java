edce6ea682575b3083eb33a079bc555d0a8e2d83
campitos <rapidclimate@gmail.com>
2018-02-08 17:43:00 -0600

    el tercero

--- app/src/main/java/diabla/org/diabla/MainActivity.java
+++ app/src/main/java/diabla/org/diabla/MainActivity.java
@@ -2,6 +2,8 @@ package diabla.org.diabla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Maldito viruscxcxcxcx
+
+              findViewById(R.id.enviar).setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View view) {
+                      Toast.makeText(getApplicationContext(),"Tu mensaje se envio!", Toast.LENGTH_LONG).show();
+                  }
+              });
+
     }
 }
